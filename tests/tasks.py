RealOrangeOne/django-tasks

Add support for receiving task context (#174)

tests/tasks.py
@@ -1,7 +1,7 @@
 import time
 from typing import Any
 
-from django_tasks import task
+from django_tasks import TaskContext, task
 
 
 @task()
@@ -70,3 +70,14 @@ def hang() -> None:
 @task()
 def sleep_for(seconds: float) -> None:
     time.sleep(seconds)
+
+
+@task(takes_context=True)
+def get_task_id(context: TaskContext) -> str:
+    return context.task_result.id
+
+
+@task(takes_context=True)
+def test_context(context: TaskContext, attempt: int) -> None:
+    assert isinstance(context, TaskContext)
+    assert context.attempt == attempt

RealOrangeOne/django-tasks-rq

tests/tasks.py
@@ -1,7 +1,7 @@
 import time
 from typing import Any
 
-from django_tasks_rq import task
+from django_tasks_rq import TaskContext, task
 
 
 @task()
@@ -60,3 +60,14 @@ def hang() -> None:
 @task()
 def sleep_for(seconds: float) -> None:
     time.sleep(seconds)
+
+
+@task(takes_context=True)
+def get_task_id(context: TaskContext) -> str:
+    return context.task_result.id
+
+
+@task(takes_context=True)
+def test_context(context: TaskContext, attempt: int) -> None:
+    assert isinstance(context, TaskContext)
+    assert context.attempt == attempt