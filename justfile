abnerpc/django-tasks-db

Fix #42 Use modules in justfile (#43)

justfile
@@ -3,16 +3,17 @@
   just --list
 
 test *ARGS:
-    ./manage.py makemigrations --check --noinput
-    coverage run --source=django_tasks_db manage.py test {{ ARGS }}
-    coverage report
-    coverage html
+    python -m manage check
+    python -m manage makemigrations --dry-run --check --noinput
+    python -m coverage run --source=django_tasks_db -m manage test --shuffle --noinput {{ ARGS }}
+    python -m coverage report
+    python -m coverage html
 
 format:
-    ruff check django_tasks_db tests --fix
-    ruff format django_tasks_db tests
+    python -m ruff check django_tasks_db tests --fix
+    python -m ruff format django_tasks_db tests
 
 lint:
-    ruff check django_tasks_db tests
-    ruff format django_tasks_db tests --check
-    mypy django_tasks_db tests
+    python -m ruff check django_tasks_db tests
+    python -m ruff format django_tasks_db tests --check
+    python -m mypy django_tasks_db tests

RealOrangeOne/django-tasks-rq

justfile
@@ -3,16 +3,17 @@
   just --list
 
 test *ARGS:
-    ./manage.py makemigrations --check --noinput
-    coverage run --source=django_tasks_rq manage.py test {{ ARGS }}
-    coverage report
-    coverage html
+    python -m manage check
+    python -m manage makemigrations --dry-run --check --noinput
+    python -m coverage run --source=django_tasks_rq -m manage test --shuffle --noinput {{ ARGS }}
+    python -m coverage report
+    python -m coverage html
 
 format:
-    ruff check django_tasks_rq tests --fix
-    ruff format django_tasks_rq tests
+    python -m ruff check django_tasks_rq tests --fix
+    python -m ruff format django_tasks_rq tests
 
 lint:
-    ruff check django_tasks_rq tests
-    ruff format django_tasks_rq tests --check
-    mypy django_tasks_rq tests
+    python -m ruff check django_tasks_rq tests
+    python -m ruff format django_tasks_rq tests --check
+    python -m mypy django_tasks_rq tests